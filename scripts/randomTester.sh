ed0d3cc4b6a76902eec26012fa9aabb287217db3
emoney <emoney33@gmail.com>
2013-05-02 13:48:23 -0500

    Added more scripts and working with new datasets

---
+++ scripts/randomTester.sh
@@ -0,0 +1,10 @@
+export CLASSPATH="${CLASSPATH}:.:${MINORTHIRD-.}/class:${MINORTHIRD-.}/lib/*:${MINORTHIRD-.}/lib/minorThirdIncludes.jar"
+export CLASSPATH="${CLASSPATH}:${MINORTHIRD-.}/lib/mixup:${MINORTHIRD-.}/config"
+export MONTYLINGUA="${MINORTHIRD-.}/lib/montylingua"
+
+java  -Xmx1000M edu.cmu.minorthird.ui.TrainTestExtractor -labels random_train_loader -test random_test_loader -spanType true_name -learner "new Recommended.CRFAnnotatorLearner()" -output "predicted_name" -showTestDetails true -saveAs randomResults
+
+#VPHMMLearner()
+#CRFAnnotatorLearner()
+#HMMAnnotatorLearner()
+#VPTagLearner()
--- scripts/trainer.sh
+++ scripts/trainer.sh
@@ -2,4 +2,4 @@ export CLASSPATH="${CLASSPATH}:.:${MINORTHIRD-.}/class:${MINORTHIRD-.}/lib/*:${M
 export CLASSPATH="${CLASSPATH}:${MINORTHIRD-.}/lib/mixup:${MINORTHIRD-.}/config"
 export MONTYLINGUA="${MINORTHIRD-.}/lib/montylingua"
 
-java  -Xmx1000M edu.cmu.minorthird.ui.TrainExtractor -labels enron0_names -spanType name -learner "new Recommended.HMMAnnotatorLearner()" -saveAs trainedOnEnron0_names
+java  -Xmx1000M edu.cmu.minorthird.ui.TrainExtractor -labels train_loader -spanType true_name -learner "new Recommended.HMMAnnotatorLearner()" -saveAs train_trained
