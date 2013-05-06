94168c1d9045c4e409f3b4c28cef4122c1b11f45
Anne Flinchbaugh <aflinchb@cs.utexas.edu>
2013-05-06 12:19:54 -0500

    date time scala regex file added

---
+++ GetDateTime.scala
@@ -0,0 +1,55 @@
+import scala.io.Source
+
+// possible iterations of DateRE:
+//   -just 3 letter abbr
+//   -3 letter abbr + full endings
+// adding Day xx?
+// adding \d\d
+val DateRE = """\b(((Mon|Tue|Wed|Thu|Fri|Sat|Sun|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)(day|sday|nesday|rsday|urday|uary|ruary|ch|il|e|y|ust|tember|ober|ember)?)|(Day \d\d?)|((?![01]\d[: ]|:[01]\d)[01]\d))\b""".r
+
+
+// possible iterations of TimeRE:
+//   -just xx:xx:xx
+//   - above + xx:xx PM|AM
+//   - making AM|PM optional
+//   - making 1 digit optional in XX:xx PM|AM
+//   - including x(am|pm|AM|PM)
+//   - adding white space (\s?) after \d\d
+val TimeRE= """\b((\d\d:\d\d:\d\d)|(\d\d?:\d\d\s?(AM|PM)?)|(\d(am|pm|AM|PM)))\b""".r
+
+
+val dirName = args(0)
+
+val fileList = new java.io.File(dirName).listFiles //.filter(_.getName.endsWith(".txt"))
+
+
+for ( file <- fileList) yield {
+val document = scala.io.Source.fromFile(file,"8859_1").getLines.toList.mkString(" ")
+
+val dates = {for {DateRE(date,_,_,_,_,_) <- DateRE findAllIn document} yield (date) }.toList
+
+val times = {for {TimeRE(time,_,_,_,_,_) <- TimeRE findAllIn document} yield (time) }.toList
+
+var prevIndex = -1
+for (date <- dates) yield {
+		val index = document.indexOfSlice(date,prevIndex+1)
+		val length=  date.length
+		
+		println(file.getName +  " " + index + " " + length  )
+		//println(document.slice(index,index + length))
+	
+		prevIndex = index
+}
+
+prevIndex = -1
+for (time <- times) yield{
+	val index = document.indexOfSlice(time,prevIndex+1)
+	val length = time.length
+
+	println(file.getName +  " " + index + " " + length  )
+	//println(document.slice(index,index + length))
+	prevIndex = index
+}
+
+//println(file.getName + " " + first_last.length)
+}
