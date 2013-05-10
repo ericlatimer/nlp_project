6e3c4d46bc28952d78c0de27ccc583800f0c0165
emoney <emoney33@gmail.com>
2013-05-10 13:26:37 -0500

    modified all.sh for crfs

--- scripts/crf/all.sh
+++ scripts/crf/all.sh
@@ -1,5 +1,5 @@
-./scripts/crf/meetingsTester.sh
-./scripts/crf/randomTester.sh
-./scripts/crf/newsgroupTester.sh
-./scripts/crf/sriTester.sh
-./scripts/crf/jobsTester.sh
+bash /u/elatimer/nlp_project/scripts/crf/meetingsTester.sh
+bash /u/elatimer/nlp_project/scripts/crf/randomTester.sh
+bash /u/elatimer/nlp_project/scripts/crf/newsgroupTester.sh
+bash /u/elatimer/nlp_project/scripts/crf/sriTester.sh
+bash /u/elatimer/nlp_project/scripts/crf/jobsTester.sh
